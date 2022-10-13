8f3ddc2e96f0809d0b8fb9a53853b22e1bd15d18
1J-Choi <chldnjswp110@naver.com>
2022-10-13 13:46:30 +0900

    Member 관련 back-end 구조

--- src/main/java/com/TCG/entity/Member.java
+++ src/main/java/com/TCG/entity/Member.java
@@ -1,16 +1,37 @@
 package com.TCG.entity;
 
+import com.TCG.Dto.MemberFormDto;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "member")
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
 
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String adress;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member();
+        member.setName(member.getName());
+        member.setEmail(member.getEmail());
+        String password =passwordEncoder.encode(member.getPassword());
+        member.setAdress(member.getAdress());
+        return member;
+    }
 }
---
+++ src/main/java/com/TCG/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.TCG.repository;
+
+import com.TCG.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Member findByEmail(String email);
+}
